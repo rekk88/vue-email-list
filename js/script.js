0a5e6ace7c18b5d5bcc1489b8206c69866c3997e
maicol <maicol.matteoli@gmail.com>
2021-07-02 15:30:08 +0200

    add : una mail c'è

--- js/script.js
+++ js/script.js
@@ -2,14 +2,28 @@ var app = new Vue({
     el:"#app",
 
     data: {
-
+        indirizzo: ""
     },
     mounted() {
-        this.test()
+        // this.test()
+        axios 
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(email => {
+                this.indirizzo = email.data.response;
+            });
     },
     methods: {
         test(){
             console.log("sei un grande");
+        },
+        getEmail(){
+            axios 
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(email => {
+                    this.indirizzo = email.data.response;
+                });
+            
+            console.log(this.indirizzo);
         }
     },
 });
