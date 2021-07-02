4cacf52f29b0b3bc95502478eb7e581aed450e5f
maicol <maicol.matteoli@gmail.com>
2021-07-02 14:43:50 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el:"#app",
+
+    data: {
+
+    },
+    mounted() {
+        this.test()
+    },
+    methods: {
+        test(){
+            console.log("sei un grande");
+        }
+    },
+});
+
